006402cedf866d684a970707b0c14982e4079617
kfisherking <kfisherking@outlook.com>
2025-08-24 16:08:34 +0100

    initial-commit

---
+++ scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var score = 0
+
+@onready var scorelabel: Label = $scorelabel
+
+func add_point():
+    score += 1
+    scorelabel.text = "you collected " + str(score) + " coins out of 12."
+ 
---
+++ scripts/slime.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+@onready var raycastright: RayCast2D = $raycastright
+@onready var raycastleft: RayCast2D = $raycastleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    if raycastright.is_colliding():
+        direction = -1 
+        animated_sprite_2d.flip_h = true
+    if raycastleft.is_colliding():
+         direction = 1
+         animated_sprite_2d.flip_h = false
+    position.x += direction * SPEED * delta
