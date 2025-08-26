11162dcf3086032852501fd281eb98f4cdd3af80
kfisherking <kfisherking@outlook.com>
2025-08-26 18:11:43 +0100

    push

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -8,5 +8,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_point()
-	animation_player.play("pickup")
+    game_manager.add_point()
+    animation_player.play("pickup")
--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -4,11 +4,11 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body):
-	print("you died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+    print("you died!")
+    Engine.time_scale = 0.5
+    body.get_node("CollisionShape2D").queue_free()
+    timer.start()
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1
-	get_tree().reload_current_scene()
+    Engine.time_scale = 1
+    get_tree().reload_current_scene()
